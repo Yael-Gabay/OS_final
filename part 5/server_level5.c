65ac551d5f2a59f5fd5c6af4160088571ae49d4a
yael <yael4231@gmail.com>
2024-06-25 18:47:12 +0300

    first commit

--- part 5/server_level5.c
+++ part 5/server_level5.c
@@ -6,38 +6,61 @@
 #include <netinet/in.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
-#include <time.h>
+#include <poll.h>
 #include <pthread.h>
+#include <errno.h>
+#include <time.h>
 
 #define PORT 8080
+#define MAX_EVENTS 50
 #define BUFFER_SIZE 1024
 #define ll long long
 
-// Function to calculate (base^exp) % mod
+typedef void* (*handler_t)(int fd);
+
+typedef struct {
+    int fd;
+    handler_t handler;
+    pthread_mutex_t fd_lock; // Mutex to synchronize access to fd
+} event_source_t;
+
+typedef struct {
+    struct pollfd fds[MAX_EVENTS];
+    event_source_t sources[MAX_EVENTS];
+    int count;
+    pthread_mutex_t lock;
+} proactor_t;
+
+ll highest_prime = 0;
+int requestCounter = 0;
+FILE *logFile;
+pthread_mutex_t prime_mutex; // הוספת מנעול
+
+// פונקציה לחישוב (base^exp) % mod
 ll powerMod(ll base, ll exp, ll mod) {
     ll result = 1;
     base = base % mod;
     while (exp > 0) {
-        if (exp % 2 == 1) // if exp is odd
+        if (exp % 2 == 1) // אם exp אי-זוגי
             result = (result * base) % mod;
-        exp = exp >> 1; // divide exp by 2
+        exp = exp >> 1; // חלוקה ב-2
         base = (base * base) % mod;
     }
     return result;
 }
 
-// Function to perform the core Miller-Rabin test
+// פונקציה שעושה את הבדיקה העיקרית של מילר-רבין
 bool millerTest(ll d, ll n) {
-    ll a = 2 + rand() % (n - 4); // random number in [2, n-2]
-    ll x = powerMod(a, d, n);    // Compute (a^d) % n
+    ll a = 2 + rand() % (n - 4); // משתנה a: בחירת מספר אקראי בתחום [2, n-2]
+    ll x = powerMod(a, d, n);    // משתנה x: חישוב (a^d) % n
 
     if (x == 1 || x == n - 1)
         return true;
 
-    // Repeat squaring x^2 % n
-    while (d != n - 1) {
+    // חישוב חוזר של x^2 % n
+    while (d != n - 1) { // משתנה d מחושב תחילה כ- n - 1 ומחולק ב-2 עד שהוא אי-זוגי
         x = (x * x) % n;
-        d *= 2; // s (number of steps)
+        d *= 2; // s (מספר הצעדים) 
 
         if (x == 1)
             return false;
@@ -47,7 +70,7 @@ bool millerTest(ll d, ll n) {
     return false;
 }
 
-// Function to check primality using Miller-Rabin test
+// פונקציה לבדוק ראשוניות בשיטת מילר-רבין
 bool isPrime(ll n, int k) {
     if (n <= 1 || n == 4)
         return false;
@@ -65,125 +88,136 @@ bool isPrime(ll n, int k) {
     return true;
 }
 
-typedef struct {
-    int fd;
-    pthread_mutex_t lock;
-    ll highest_prime;
-} client_info_t;
+proactor_t* proactor_init() {
+    proactor_t* proactor = malloc(sizeof(proactor_t));
+    if (!proactor) return NULL;
+    memset(proactor, 0, sizeof(proactor_t));
+    pthread_mutex_init(&proactor->lock, NULL);
+    return proactor;
+}
 
-pthread_mutex_t prime_mutex = PTHREAD_MUTEX_INITIALIZER;
-int requestCounter = 0;
-ll highest_prime = 0;
+void* event_handler_wrapper(void* arg) {
+    event_source_t* source = (event_source_t*)arg;
+    pthread_mutex_lock(&source->fd_lock);
+    if (source->fd != -1) {
+        source->handler(source->fd);
+        close(source->fd); // Close fd and mark as closed
+        source->fd = -1;
+    }
+    pthread_mutex_unlock(&source->fd_lock);
+    return NULL;
+}
 
-void* client_handler(void* arg) {
-    client_info_t* client_info = (client_info_t*)arg;
-    int fd = client_info->fd;
-    ll* highest_prime_ptr = &(client_info->highest_prime);
+void proactor_add_fd(proactor_t* proactor, int fd, handler_t handler) {
+    pthread_mutex_lock(&proactor->lock);
+    if (proactor->count < MAX_EVENTS) {
+        proactor->fds[proactor->count].fd = fd;
+        proactor->fds[proactor->count].events = POLLIN;
+        proactor->sources[proactor->count].fd = fd;
+        proactor->sources[proactor->count].handler = handler;
+        pthread_mutex_init(&proactor->sources[proactor->count].fd_lock, NULL);
+        proactor->count++;
+    }
+    pthread_mutex_unlock(&proactor->lock);
+}
 
-    char buffer[BUFFER_SIZE];
-    int valread;
-
+void proactor_run(proactor_t* proactor) {
     while (1) {
-        valread = read(fd, buffer, BUFFER_SIZE - 1);
-        if (valread > 0) {
-            buffer[valread] = '\0';
-            ll num = atoll(buffer);
-            srand(time(0)); // Initialize random number generator
-            int prime = isPrime(num, 5);
-            
-            pthread_mutex_lock(&prime_mutex);
-            requestCounter++;
-            if (prime) {
-                if (num > highest_prime) {
-                    highest_prime = num;
-                    printf("Request #%d: %lld is prime. Highest prime: %lld\n", requestCounter, num, highest_prime);
+        int ret = poll(proactor->fds, proactor->count, -1);
+        if (ret > 0) {
+            for (int i = 0; i < proactor->count; i++) {
+                if (proactor->fds[i].revents & POLLIN) {
+                    if (proactor->fds[i].fd == proactor->sources[0].fd) {  // Listening socket
+                        struct sockaddr_in client_addr;
+                        socklen_t client_len = sizeof(client_addr);
+                        int client_fd = accept(proactor->fds[i].fd, (struct sockaddr *)&client_addr, &client_len);
+                        if (client_fd > 0) {
+                            proactor_add_fd(proactor, client_fd, proactor->sources[0].handler);
+                        }
+                    } else {
+                        pthread_t thread;
+                        pthread_create(&thread, NULL, event_handler_wrapper, &proactor->sources[i]);
+                        pthread_detach(thread);
+                    }
                 }
-                *highest_prime_ptr = highest_prime;
-            } else {
-                printf("Request #%d: %lld is not prime.\n", requestCounter, num);
             }
-
-            sprintf(buffer, "Request #%d: %lld is %sprime. Highest prime so far: %lld\n", requestCounter, num, prime ? "" : "not ", highest_prime);
-            send(fd, buffer, strlen(buffer), 0);
-            pthread_mutex_unlock(&prime_mutex);
-        } 
-        if (valread <= 0) {
-            printf("Client disconnected: socket fd %d\n", fd);
-            close(fd);
-            break;
         }
     }
+}
 
-    pthread_mutex_lock(&client_info->lock);
-    client_info->fd = -1;
-    pthread_mutex_unlock(&client_info->lock);
+void* client_handler(int fd) {
+    char buffer[BUFFER_SIZE];
+    int valread;
+    while ((valread = read(fd, buffer, BUFFER_SIZE - 1)) > 0) {
+        buffer[valread] = '\0';
+        long long num = atoll(buffer);
+        srand(time(0)); // אתחול מחולל המספרים האקראיים
+        int prime = isPrime(num, 5);
+        pthread_mutex_lock(&prime_mutex);
+        requestCounter++;
+        if (prime) {
+            if (num > highest_prime) highest_prime = num;
+            fprintf(logFile, "Request #%d: %lld is prime. Highest prime: %lld\n", requestCounter, num, highest_prime);
+        } else {
+            fprintf(logFile, "Request #%d: %lld is not prime.\n", requestCounter, num);
+        }
+        fflush(logFile);
 
-    free(client_info);
+        sprintf(buffer, "Request #%d: %lld is %sprime. Highest prime so far: %lld\n", requestCounter, num, prime ? "" : "not ", highest_prime);
+        send(fd, buffer, strlen(buffer), 0);
+        pthread_mutex_unlock(&prime_mutex);
+    }
+
+    if (valread <= 0) {
+        printf("Client disconnected: socket fd %d\n", fd);
+        close(fd);
+    }
     return NULL;
 }
 
 int main() {
-    int server_fd, new_socket, valread;
-    struct sockaddr_in address;
-    int addrlen = sizeof(address);
-
-    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+    int listen_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (listen_fd == 0) {
         perror("socket failed");
         exit(EXIT_FAILURE);
     }
 
     int opt = 1;
-    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt))) {
+    if (setsockopt(listen_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt))) {
         perror("setsockopt");
         exit(EXIT_FAILURE);
     }
 
-    address.sin_family = AF_INET;
-    address.sin_addr.s_addr = INADDR_ANY;
-    address.sin_port = htons(PORT);
+    struct sockaddr_in addr;
+    memset(&addr, 0, sizeof(addr));
+    addr.sin_family = AF_INET;
+    addr.sin_addr.s_addr = INADDR_ANY;
+    addr.sin_port = htons(PORT);
 
-    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
+    if (bind(listen_fd, (struct sockaddr *)&addr, sizeof(addr)) < 0) {
         perror("bind failed");
         exit(EXIT_FAILURE);
     }
 
-    if (listen(server_fd, 10) < 0) {
+    if (listen(listen_fd, 10) < 0) {
         perror("listen");
         exit(EXIT_FAILURE);
     }
 
-    printf("The server is waiting for connections on port %d...\n", PORT);
-
-    while (1) {
-        if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen)) < 0) {
-            perror("accept");
-            continue;
-        }
-        
-        printf("New connection: socket fd is %d, ip is: %s, port: %d\n", new_socket, inet_ntoa(address.sin_addr), ntohs(address.sin_port));
-
-        // Handle the new client in a new thread
-        pthread_t thread;
-        client_info_t* client_info = (client_info_t*)malloc(sizeof(client_info_t));
-        if (!client_info) {
-            perror("Failed to allocate memory for client_info");
-            close(new_socket);
-            continue;
-        }
-
-        client_info->fd = new_socket;
-        client_info->highest_prime = 0;
-        pthread_mutex_init(&client_info->lock, NULL);
-
-        if (pthread_create(&thread, NULL, client_handler, (void*)client_info) != 0) {
-            perror("Failed to create thread for client");
-            close(new_socket);
-            free(client_info);
-            continue;
-        }
-
-        pthread_detach(thread);
+    logFile = fopen("server_log.txt", "w");
+    if (!logFile) {
+        perror("Failed to open log file");
+        exit(EXIT_FAILURE);
     }
 
+    // אתחול המנעול
+    pthread_mutex_init(&prime_mutex, NULL);
+
+    proactor_t* proactor = proactor_init();
+    proactor_add_fd(proactor, listen_fd, client_handler);
+    proactor_run(proactor);
+
+    fclose(logFile);
+    pthread_mutex_destroy(&prime_mutex); // השמדת המנעול
     return 0;
 }
